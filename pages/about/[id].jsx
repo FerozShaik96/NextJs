50fe89253e79c72b9a2ff397de1f76fbd1fcf4e3
FerozShaik96 <ferozshaik1997@gmail.com>
2023-09-08 21:24:16 +0530

    Dynamic Page Routing

--- pages/about/[id].jsx
+++ pages/about/[id].jsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 function Developer() {
+  const router = useRouter();
+  const urlId = router.query.id;
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+  const data = details.filter((item) => item.id === +urlId);
   return (
     <div>
-      <h1>Feroz Full Stack Developer</h1>
+      {data.length > 0 ? (
+        data.map((item) => (
+          <h1 key={item.id}>
+            {item.name} ----{item.role}
+          </h1>
+        ))
+      ) : (
+        <h1> {`Developer doesn't exist`}</h1>
+      )}
     </div>
   );
 }
